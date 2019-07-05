6867d53723e821e8d15c9c606ee31d5ee4674531
John <jonathan.willian3443@gmail.com>
2019-07-05 18:22:14 -0300

    testes

---
+++ app/static/js/script.js
@@ -0,0 +1,9 @@
+/*
+
+Javascript - MobThon
+
+Aqui ficará concentrado todo código referente ao JAVASCRIPT do código. é uma linguagem de programação interpretada 
+de alto nível, caracterizada também, como dinâmica, fracamente tipada, prototype-based e multi-paradigma. Juntamente 
+com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.
+
+*/ 
