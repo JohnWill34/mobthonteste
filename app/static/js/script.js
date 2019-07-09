ed9354418ec48194de811a75f8cdba499bab9374
John <jonathan.willian3443@gmail.com>
2019-07-09 00:02:28 -0300

    testes1.1.9

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -7,3 +7,14 @@ de alto nível, caracterizada também, como dinâmica, fracamente tipada, protot
 com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.
 
 */ 
+
+(function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) menu.classList.add('menuScroll');
+        else menu.classList.remove('menuScroll');
+        if (window.scrollY > 0) botao.classList.add('botaoScroll');
+        else botao.classList.remove('botaoScroll');
+    });
+})();
+
+console.log("Hello world!");
